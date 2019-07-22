29899e1b96d3224abf6249cd1fbe31ab8f2d22ad
Usman Shahid <usman.shahid@Intechww.com>
2019-07-22 18:09:15 +0500

    Added basic CLI

--- plumber/cli.py
+++ plumber/cli.py
@@ -1,6 +1,11 @@
 import click
 import click_log
 import plumber.common
+from plumber.core import PlumberPlanner
+from plumber.io import YamlEnvFileStore
+from plumber.common import PATH
+
+DEFAULT_CONFIG_PATH = 'plumber.yml'
 
 click_log.basic_config(plumber.common.LOG)
 
@@ -14,14 +19,36 @@ def cli():
   pass
 
 
+@click.command('report')
+@click.option('--cfg', '-c', help='Path tho plumber config file')
 @click_log.simple_verbosity_option(plumber.common.LOG)
-def get_report():
-  pass
+def get_report(cfg):
+  if cfg is None:
+    cfg = DEFAULT_CONFIG_PATH
+  config_store = YamlEnvFileStore()
+  config_store.configure({PATH: cfg})
+  config = config_store.get_data()
+  if config is None or len(config) == 0:
+    click.echo('Error: Configuration file empty or not found')
+    return
+  planner = PlumberPlanner(config)
+  click.echo(planner.get_analysis_report())
 
 
+@click.command('go')
+@click.option('--cfg', '-c', help='Path tho plumber config file')
 @click_log.simple_verbosity_option(plumber.common.LOG)
-def execute():
-  pass
+def execute(cfg):
+  if cfg is None:
+    cfg = DEFAULT_CONFIG_PATH
+  config_store = YamlEnvFileStore()
+  config_store.configure({PATH: cfg})
+  config = config_store.get_data()
+  if config is None or len(config) == 0:
+    click.echo('Error: Configuration file empty or not found')
+    return
+  planner = PlumberPlanner(config)
+  planner.execute()
 
 
 cli.add_command(get_report)
